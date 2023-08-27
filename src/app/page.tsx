1d79720bbbf575cd893a80bffef4d5435675e2e5
Matthew Saxe <mattsaxe17@gmail.com>
2023-08-27 14:40:45 -0500

    Add example text for development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             intro="Hello I'm"
             name='Matt Saxe'
             constant='I'
-            characteristics={['do cool shit every day']}
+            characteristics={['build software', 'record podcasts', 'read cool books', 'practice MMA']}
             description="I'm a software engineer in Dallas, TX. Unfortunately, I am currently not building any software for clients. I've been working very hard on some other projects though, so feel free to check back here to see what I'm up to. Big things are coming."
             cta={{
                 url: '',
