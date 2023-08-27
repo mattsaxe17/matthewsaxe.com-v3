1bda8d521761748a252a71039a2122837bc0ab91
Matthew Saxe <mattsaxe17@gmail.com>
2023-08-27 16:13:12 -0500

    Upgrade text cycler to ensure proper sycning of text and animations

--- src/components/TextCycler.tsx
+++ src/components/TextCycler.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type TextCyclerProps = {
     iterations: Array<string>;
@@ -8,15 +8,21 @@ type TextCyclerProps = {
 
 export default function Hero({ iterations }: TextCyclerProps) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [animationClass, setAnimationClass] = useState('');
 
     setInterval(() => {
         setCurrentIndex(currentIndex === iterations.length - 1 ? 0 : currentIndex + 1);
     }, 3500);
 
+    useEffect(() => {
+        // This just ensures that the rotation stays in sync with the changing text (Kind of hacky \:)
+        setAnimationClass(`animate-[rotateDown_ease-in-out_3500ms_infinite]`);
+    }, [currentIndex]);
+
     return (
         <div id='outer'>
-            {/* Kind of hacky \: */}
-            <div id='inner' className='animate-[rotateDown_ease-in-out_3500ms_infinite] whitespace-nowrap'>
+            {/* Also kind of hacky \: */}
+            <div id='inner' className={`${animationClass} whitespace-nowrap`}>
                 {iterations[currentIndex]}
             </div>
         </div>
