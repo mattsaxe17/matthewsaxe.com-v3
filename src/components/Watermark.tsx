46389ec83a1c50f555408c8ee72297a5da9ff2c7
Matthew Saxe <mattsaxe17@gmail.com>
2023-08-27 15:11:57 -0500

    Move watermark backwards

--- src/components/Watermark.tsx
+++ src/components/Watermark.tsx
@@ -4,7 +4,7 @@ type WatermarkProps = {
 
 export default function Hero({ text }: WatermarkProps) {
     return (
-        <div className='p-0 m-0 fixed z-0 text-[48vw] leading-tight bottom-4 opacity-5 font-mono rotate-[-17deg]'>
+        <div className='p-0 m-0 fixed -z-20 text-[48vw] leading-tight bottom-4 opacity-5 font-mono rotate-[-17deg]'>
           {text}
         </div>
     );
