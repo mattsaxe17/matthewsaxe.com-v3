195d7c934f5b9e36705be4f16197330174eafbda
Matthew Saxe <mattsaxe17@gmail.com>
2023-09-03 09:25:23 -0500

    Implement section header

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,17 @@
+type HeaderProps = {
+    prefix?: string;
+    title: string;
+};
+
+function SectionHeader({ prefix, title }: HeaderProps) {
+    return (
+        <div className='flex items-center justify-center pb-10 max-w-screen-xl m-auto'>
+            <h2 className='font-bold text-3xl w-full flex items-center whitespace-nowrap gap-3 px-2 after:content-[""] after:block after:relative after:h-px after:bg-primary after:w-full'>
+                <span className='text-primary text-xl'>{prefix}</span>
+                {title}
+            </h2>
+        </div>
+    );
+}
+
+export default SectionHeader;
