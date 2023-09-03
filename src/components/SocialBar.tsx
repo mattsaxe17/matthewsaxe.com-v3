b444fe956777600c9c5b7725a68583e42708bbd9
Matthew Saxe <mattsaxe17@gmail.com>
2023-09-03 09:28:18 -0500

    Fix social bar position

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -6,11 +6,11 @@ type SocialBarProps = {
 };
 
 function SocialBar({ location, fixed, desktopOnly, socials }: SocialBarProps) {
-    const conditionalClasses = `${location === 'right' ? 'right-6' : location === 'left' ? 'left-6' : ''} ${fixed ? 'fixed' : ''} ${desktopOnly ? 'hidden md:flex' : ''}`;
+    const conditionalClasses = `${location === 'right' ? 'right-6' : location === 'left' ? 'left-6' : ''} ${fixed ? 'fixed' : 'absolute'} ${desktopOnly ? 'hidden md:flex' : ''}`;
 
     return (
         <div
-            className={`${conditionalClasses} text-secondary-text dark:text-dark-secondary-text w-12 absolute bottom-0 flex flex-col items-center z-20 after:content-[''] after:left-2/4 after:block after:w-0.5 after:h-24 after:bg-secondary-text after:dark:text-dark-secondary-text`}
+            className={`${conditionalClasses} text-secondary-text dark:text-dark-secondary-text w-12 bottom-0 flex flex-col items-center z-20 after:content-[''] after:left-2/4 after:block after:w-0.5 after:h-24 after:bg-secondary-text after:dark:text-dark-secondary-text`}
         >
             {socials.map(social => (
                 <div key={social.link} className='text-xl hover:text-primary hover:text-3xl transition-all duration-150 py-3 cursor-pointer'>
