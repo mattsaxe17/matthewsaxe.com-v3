123604eb3633f1ef4d0337abd6b0e6d88fc5985f
Matthew Saxe <mattsaxe17@gmail.com>
2023-09-12 19:10:15 -0500

    Complete about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from '@/components/Hero';
 import SectionHeader from '@/components/SectionHeader';
 import Watermark from '@/components/Watermark';
 
+const whatIveBeenUpTo = ['Vue', 'React', 'Nuxt.js/Next.js', 'Sass', '.NET Core', 'SQL Server', 'Strapi', 'Handlebars', 'Docker', 'Netlify functions'];
+
 export default function Home() {
     return (
         <>
@@ -18,9 +20,38 @@ export default function Home() {
                 }}
             ></Hero>
 
-            <SectionHeader prefix='00.' title='About Me' anchor='about'></SectionHeader>
+            <section className='px-6 flex flex-col items-center max-w-screen-xl mx-auto'>
+                <SectionHeader prefix='00.' title='About Me' anchor='about'></SectionHeader>
 
-            {/* <p>If you know me, you know I have long list of interests. I&apos;m passionate about fitness and martial arts, read often, like to cook, and have recently adopted an animal based diet. As you probably guessed, I also have a deep passion for writing software. From websites to FitBit watch faces, I am passionate about problem solving through the medium of code. It all started in 11th grade, where I taught myself HTML, CSS, and Javascript in study hall... These days, I am a software engineer professionally. I am not currently taking any clients, but I have created projects for clients in the past. My experience extends from front-end design to servers and databases, and I love working with complex math, data structures, and algorithms. From simple websites to complex web apps, check out my Github to see what I&apos;m up to these days. (I am still very active on Github) I&apos;m always open to expand my network, so reach out however you&apos;d like - I&apos;d love to talk.</p> */}
+                <p className='text-secondary-text dark:text-dark-secondary-text mb-10'>
+                    If you know me, you know I have long list of interests. I&apos;m passionate about fitness and martial arts, read often, like to cook, and have recently adopted an animal based diet. As you probably guessed, I also have a deep
+                    passion for writing software. From websites to FitBit watch faces, I am passionate about problem solving through the medium of code. It all started in 11th grade, where I taught myself HTML, CSS, and Javascript in study hall...
+                    <br />
+                    <br />
+                    These days, I am a software engineer professionally. I am not currently taking any clients, but I have created projects for clients in the past. My experience extends from front-end design to servers and databases, and I love
+                    working with complex math, data structures, and algorithms. From simple websites to complex web apps, check out my Github to see what I&apos;m up to these days. (I am still very active on Github)
+                    <br />
+                    <br />
+                    I&apos;m always open to expand my network, so reach out however you&apos;d like - I&apos;d love to talk.
+                </p>
+
+                <div className='flex flex-col items-center lg:flex-row mb-96'>
+                    <div className='max-w-xl'>
+                        <p className='mb-2 w-full'>What I&apos;ve been up to lately:</p>
+                        <ul className='flex flex-wrap mb-10'>
+                            {whatIveBeenUpTo.map(item => (
+                                <li key={item} className='before:content-[url(/arrow.svg);] before:pr-2 text-secondary-text dark:text-dark-secondary-text w-2/4 pl-2'>
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className='w-72 rounded-lg border-primary border-4 lg:mr-16'>
+                        <img src='/headshot.jpg' alt='me' className='grayscale transition-all duration-500 translate-x-8 translate-y-10 rounded-md hover:translate-x-0 hover:translate-y-0 hover:scale-110 hover:grayscale-0' />
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -6,8 +6,8 @@ type HeaderProps = {
 
 function SectionHeader({ prefix, title, anchor }: HeaderProps) {
     return (
-        <div id={anchor} className='flex items-center justify-center pb-10 max-w-screen-xl m-auto'>
-            <h2 className='font-bold text-3xl w-full flex items-center whitespace-nowrap gap-3 px-2 after:content-[""] after:block after:relative after:h-px after:bg-primary after:w-full'>
+        <div id={anchor} className='flex items-center justify-center pb-10 m-auto w-full'>
+            <h2 className='font-bold text-3xl w-full flex items-center whitespace-nowrap gap-3 after:content-[""] after:block after:relative after:h-px after:bg-primary after:w-full'>
                 <span className='text-primary text-xl'>{prefix}</span>
                 {title}
             </h2>
