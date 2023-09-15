f082fd0387e37c95ab0a8fa69e449525d92694fd
Matthew Saxe <mattsaxe17@gmail.com>
2023-09-14 19:47:06 -0500

    Simplify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,7 @@
-type FooterProps = {
-};
-
 export default function AppBar() {
-
     return (
-        <div className='w-screen bg-primary-bg dark:bg-dark-primary-bg h-48 border-t-2 border-t-secondary-text dark:border-t-dark-secondary-text p-10'>
-          IMAGINE A FOOTER HERE
+        <div className='w-screen bg-primary-bg dark:bg-dark-primary-bg border-t-2 border-t-secondary-text dark:border-t-dark-secondary-text text-secondary-text dark:text-dark-secondary-text p-10 flex justify-center'>
+            <p className='text-sm'>© {new Date().getFullYear()} Matthew Saxe</p>
         </div>
     );
 }
