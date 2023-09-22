d5cbd71d1cc897e765b94b954cffa01ee4563517
Matthew Saxe <mattsaxe17@gmail.com>
2023-09-22 17:54:42 -0500

    Add links to project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
+import { BsGithub } from 'react-icons/bs';
+
 type ProjectProps = {
     title: string;
     imageUrl: string;
     description: string;
     techStack: Array<string>;
-    githubLink?: string;
     projectLink?: string;
+    githubLink?: string;
 };
 
 function PlatformIcon({ title, imageUrl, description, techStack, githubLink, projectLink }: ProjectProps) {
@@ -12,13 +15,25 @@ function PlatformIcon({ title, imageUrl, description, techStack, githubLink, pro
         <div className='flex flex-col shadow-2xl rounded-lg bg-primary-bg dark:bg-dark-primary-bg shadow-dark-faint-text overflow-hidden'>
             <img src={imageUrl} />
             <div className='p-5'>
-                <p className='text-primary'>Featured</p>
-                <p className='text-lg font-bold mb-1 md:text-xl'>{title}</p>
+                <div className='flex justify-between'>
+                    <p className='text-primary'>Featured</p>
+                    {githubLink && (
+                        <a href={githubLink}>
+                            <BsGithub />
+                        </a>
+                    )}
+                </div>
+
+                <p className='text-lg font-bold mb-1 md:text-xl'>{projectLink ? <Link className='hover:text-secondary dark:hover:text-dark-secondary-text' href={projectLink}>{title}</Link> : <>{title}</>} </p>
+
                 <p className='text-sm text-secondary-text dark:text-dark-secondary-text mb-2 md:text-lg'>{description}</p>
                 <div className='flex gap-3'>
-                {techStack.map(tech => (
-                    <p key={tech} className='text-sm text-primary'>{tech}</p>
-                ))}</div>
+                    {techStack.map(tech => (
+                        <p key={tech} className='text-sm text-primary'>
+                            {tech}
+                        </p>
+                    ))}
+                </div>
             </div>
         </div>
     );
